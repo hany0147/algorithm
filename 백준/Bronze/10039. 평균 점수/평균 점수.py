080209ac7850750b83dde5f5a5019c7a43a333ae
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-01-16 13:44:13 +0900

    [Bronze IV] Title: 평균 점수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,12 @@
+scores = []
+
+for i in range(0, 5):
+    score = int(input())
+    if score >= 40:
+        scores.append(score)
+    else:
+        score = 40
+        scores.append(score)
+
+score_avrg = sum(scores) / len(scores)
+print(int(score_avrg))
