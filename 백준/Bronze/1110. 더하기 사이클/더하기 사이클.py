506ff72bc1c2a8a56d57647703c95c72f059753c
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-01-17 16:36:47 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,21 +1,20 @@
 N = input()
-res = 0
+res = 100
 cnt = 0
 copy_N = int(N)
 
-if int(N) == 0:
-    print(1)
-else:
-    while int(N) != int(res):
-        if copy_N < 10:
-            copy_N = "0" + str(copy_N)           
-        num = 0 
-        n_list = [] 
-        for n in str(copy_N): 
-            n_list.append(n)
-        num = int(n_list[0]) + int(n_list[1])
-        res = n_list[1] + str(num)[-1]
-        cnt += 1
-        copy_N = int(res)
+while int(N) != int(res):
+
+    if copy_N < 10:
+        copy_N = "0" + str(copy_N)
+           
+    num = 0
+    n_list = []
+    for n in str(copy_N):
+        n_list.append(n) 
+    num = int(n_list[0]) + int(n_list[1])
+    res = n_list[1] + str(num)[-1]
+    cnt += 1 # cnt = 1
+    copy_N = int(res)
         
-    print(cnt)
+print(cnt)
