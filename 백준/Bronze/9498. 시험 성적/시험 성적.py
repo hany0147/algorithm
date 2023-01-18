f23a8697495b88e2ef2c933590b331ee0d1883a3
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-01-18 14:33:09 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if score < 60:
+    print('F')
+elif score <= 69:
+    print('D')
+elif score <= 79:
+    print('C')
+elif score <= 89:
+    print('B')
+else:
+    print('A')
