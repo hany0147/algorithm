15c0aecdd0ac13ca9d9f54157ea7f8d82c474919
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-01-18 14:22:06 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -3,10 +3,7 @@ alp_list = []
 
 for i in range(97, 123):
     alp = chr(i)
-    if alp in strings:
-        alp_list.append(strings.find(alp))
-    else:
-        alp_list.append(-1)
+    alp_list.append(strings.find(alp))
 
 
 print(*alp_list, sep = " ")
