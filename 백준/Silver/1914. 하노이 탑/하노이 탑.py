4c1bad484a6d1c3349a96b185d056eb0c505d955
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-03-29 12:45:29 +0900

    [Silver I] Title: 하노이 탑, Time: 844 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,13 +1,15 @@
-def hanoi(n, start, end):
+def hanoi(n, a, b, c):
     if n == 1:
-        print(start, end)
+        print(a, c)
         return
-    hanoi(n - 1, start, 6-start-end) # 1개를 제외한 나머지 (n-1) 원판은 2번으로 옮겨야 한다.
-    print(start, end)
-    hanoi(n - 1, 6-start-end, end)
+    hanoi(n - 1, a, c, b) # n-1개의 원판은 b로 이동
+    # 맨 아래 마지막으로 남은 원판은 c로 이동
+    print(a, c)
+    # b에 있는 원반 n - 1개를 c로 이동시켜라
+    hanoi(n - 1, b, a, c)
 
 n = int(input())
 result = 2**n -1
 print(result)
 if n <= 20:
-    hanoi(n, 1, 3)
+    hanoi(n, 1, 2, 3)
