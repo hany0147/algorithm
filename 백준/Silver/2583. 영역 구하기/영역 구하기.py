1b3cfa890253a745c1dfba8a37445b9833723958
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-05-30 21:08:24 +0900

    [Silver I] Title: 영역 구하기, Time: 56 ms, Memory: 32740 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+
+
+def dfs(x, y):
+    global cnt
+    visited[x][y] = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < m and 0<= ny < n and visited[nx][ny] == 0:
+            cnt += 1
+            dfs(nx, ny)
+
+m, n, k = map(int, input().split())
+visited = [[0] * n for _ in range(m)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+cnt = 1
+area = 0
+res = []
+
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            visited[i][j] = 1
+
+for i in range(m):
+    for j in range(n):
+        if visited[i][j] == 0:
+            area += 1
+            dfs(i, j)
+            res.append(cnt)
+            cnt = 1
+res.sort()
+print(area)
+print(*res)
