a2a4d379cd6ac580c049e8f3197bb626c05eb3b6
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-08-07 22:31:52 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 39748 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+prefix = [0 for _ in range(N + 1)]
+arr = list(map(int, input().split()))
+
+for i in range(N):
+    prefix[i + 1] = max(prefix[i] + arr[i], arr[i])
+
+prefix.pop(0)
+print(max(prefix))
