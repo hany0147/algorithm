8490eb3e3e1904e0fd309a54248afe0fecbf0731
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-08-09 13:35:42 +0900

    [Silver II] Title: 창고 다각형, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -0,0 +1,38 @@
+n = int(input())
+info = [0] * 1001
+x_lst = []
+y_lst = []
+for _ in range(n):
+    l, h = map(int, input().split())
+    info[l] = h
+    x_lst.append(l)
+    y_lst.append(h)
+
+max_h = max(y_lst)
+max_l = max(x_lst)
+
+prefix = [0] * (max_l + 2)
+suffix = [0] * (max_l + 2)
+
+max_point = []
+
+h = 0
+for i in range(1, max_l + 3):
+    if info[i] == max_h:
+        max_point.append(i)
+        break
+    h = max(h, info[i])
+    prefix[i] = prefix[i - 1] + h
+
+h = 0
+for i in range(max_l, 0 , -1):
+    if info[i] == max_h:
+        max_point.append(i)
+        break
+    h = max(h, info[i])
+    suffix[i] = suffix[i + 1] + h
+
+ans = max(prefix) + max(suffix)
+ans += (max_point[1] - max_point[0] + 1) * max_h
+
+print(ans)
