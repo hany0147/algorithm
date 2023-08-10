a83483541d2c0f2423834cb8138345bdc782b690
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-08-10 14:23:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1016 ms, Memory: 106576 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+# prefix 초기화
+prefix = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for x in range(n):
+    for y in range(n):
+        prefix[x + 1][y + 1] = prefix[x][y + 1] + prefix[x + 1][y] + graph[x][y] - prefix[x][y]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    ans = prefix[x2][y2] - prefix[x2][y1 - 1] - prefix[x1 - 1][y2] + prefix[x1 - 1][y1 - 1]
+    print(ans)
