f078528cad95e1d8c57a45e59a763ba7294f9be6
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-08-11 22:25:07 +0900

    [Silver III] Title: N과 M (6), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+tmp = []
+
+def recur(number):
+    if number == M:
+        print(*tmp)
+        return
+    for i in arr:
+        if i in tmp:
+            continue
+        if tmp:
+            if tmp[-1] > i:
+                continue
+        tmp.append(i)
+        recur(number + 1)
+        tmp.pop()
+
+recur(0)
