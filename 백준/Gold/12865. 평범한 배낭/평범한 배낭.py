053b9e35e63934fb38417c4c15965a09a8783177
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-08-16 13:38:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 2092 ms, Memory: 156756 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,25 +1,19 @@
-import sys
-input = sys.stdin.readline
+def recur(idx, weight):
 
-n, k = map(int, input().split())
-bag = [(0, 0)]
-for _ in range(n):
-    w, v = map(int, input().split())
-    bag.append((w, v))
-# 무게: bag[i][0], 가치: bag[i][1]
-dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+    if weight > K:
+        return -99999999999
+ 
+    if idx == N:
+        return 0
+    
+    if dp[idx][weight] != -1:
+        return dp[idx][weight]
 
-for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        weight = bag[i][0]
-        value = bag[i][1]
+    dp[idx][weight] = max(recur(idx + 1, weight + items[idx][0]) + items[idx][1], recur(idx + 1, weight))
 
-        # 들 수 있는 경우보다 물건의 무게가 무거울 경우
-        if j < weight:
-            # 이전 가방의 최적값 그대로 대입
-            dp[i][j] = dp[i - 1][j]
-        # 아니라면
-        else:
-            # 이전 가방의 최적 값 vs 현 물건 가치 + 이전 물건에서 무게 - 물건 무게를 했을 때의 최적 값
-            dp[i][j] = max(value + dp[i - 1][j - weight], dp[i - 1][j])
-print(dp[n][k])
+    return dp[idx][weight]
+N, K = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(N)]
+dp = [[-1 for _ in range(100_001)] for _ in range(N)]
+
+print(recur(0, 0))
