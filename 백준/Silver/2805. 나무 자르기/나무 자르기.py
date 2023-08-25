291e9e8c3a404364aa04bea13e27e1dd7a17df20
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-08-25 16:07:49 +0900

    [Silver II] Title: 나무 자르기, Time: 4564 ms, Memory: 150336 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+
+s = 0
+e = max(trees)
+
+while s <= e:
+    mid = (s + e) // 2
+    branch = 0
+    for tree in trees:
+        if mid <= tree:
+            branch += tree - mid
+    # 원하는 만큼 얻었다면,
+    if branch >= m:
+        s = mid + 1
+    # 많이 얻었다면,
+    else:
+        e = mid - 1
+print(e)
