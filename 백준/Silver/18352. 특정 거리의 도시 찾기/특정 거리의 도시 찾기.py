533c0eebd8add62675ed2c4caecb298bd201a0af
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-12-04 20:54:01 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1716 ms, Memory: 114644 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,13 +1,13 @@
+from collections import defaultdict, deque
 import sys
-from collections import deque
+input= sys.stdin.readline
 
-input = sys.stdin.readline
+dict_ = defaultdict(list)
 
 n, m, k, x = map(int, input().split())
-lst = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    lst[a].append(b)
+    dict_[a].append(b)
 
 q = deque()
 q.append([x, 0])
@@ -21,12 +21,12 @@ while q:
     if dist == k:
         result.append(node)
         flag = True
-
-    for nxt_node in lst[node]:
+        continue
+    for nxt_node in dict_[node]:
         if not visited[nxt_node]:
             visited[nxt_node] = 1
-            q.append([nxt_node, dist + 1])
-
+            alt = dist + 1
+            q.append([nxt_node, alt])
 result.sort()
 if not flag:
     print(-1)
