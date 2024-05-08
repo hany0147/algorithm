ce86feafe34dd7d082496a53adbfb3eb5f75a1f2
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2024-05-08 19:30:11 +0900

    [Bronze I] Title: 단어 공부, Time: 80 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,16 @@
-from collections import Counter
-strs = input().upper()
-
-if len(strs) != 1:
-
-    lst_strs = Counter(strs)
-
-    if lst_strs.most_common(2)[0][1] == lst_strs.most_common(2)[1][1]:
-        print('?')
+def count_number(cnt_lst, word_lst):
+    if cnt_lst.count(max(cnt_lst)) > 1:
+        return "?"
     else:
-        print(lst_strs.most_common(1)[0][0])
-else:
-    print(strs)
+        return word_lst[cnt_lst.index(max(cnt_lst))]
+        
+sentence = input().upper()
+word_lst = list(set(sentence)) 
+cnt_lst = []
+
+# 각 단어별로 개수가 몇 개인지 저장하기
+
+for word in word_lst:
+    cnt_lst.append(sentence.count(word))
+
+print(count_number(cnt_lst, word_lst))
