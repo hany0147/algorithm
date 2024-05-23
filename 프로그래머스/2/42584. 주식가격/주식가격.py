7256504f22081a79a7523e0cc0df65327d59ba12
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2024-05-23 09:37:26 +0900

    [level 2] Title: 주식가격, Time: 73.63 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -10,26 +10,25 @@ prices: 배열(주식가격)
 from collections import deque
 
 def solution(prices):
-    prices = deque(prices)
+    # prices = deque(prices)
     answer = []
-    
-    while prices:
-        price = prices.popleft()
-        sec = 0
-        for p in prices:
-            sec += 1
-            if price > p:
-        
-                break
-        answer.append(sec)
-            
+    for i in range(len(prices)):
+        cnt = 0
+        for j in range(i + 1, len(prices)):
+            cnt += 1
+            if prices[i] > prices[j]:
+                break 
+        answer.append(cnt)
+
     return answer
 
+#     while prices:
+#         price = prices.popleft()
+#         sec = 0
+#         for p in prices:
+#             sec += 1
+#             if price > p:
+        
+#                 break
+#         answer.append(sec)
 
-    # answer = []
-    # for i in range(len(prices)):
-    #     cnt = 0
-    #     for j in range(i + 1, len(prices)):
-    #         if prices[i] <= prices[j]:
-    #             cnt += 1
-    #     answer.append(cnt)
