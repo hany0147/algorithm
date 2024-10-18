23957828a10d1d69626de58e5b37155a226e5fb5
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2024-10-18 17:11:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,37 +1,29 @@
 def solution(n, arr1, arr2):
-    
-    arr1 = makeBinary(n, arr1)
-    arr2 = makeBinary(n, arr2)
-    
     new_arr = []
     
-    for x in range(n):
-        tmp = ""
-        for y in range(n):
-            if arr1[x][y] == 1 or arr2[x][y] == 1:
-                tmp += "#" 
-            else:
-                tmp += " "
-        new_arr.append(tmp)
+    def make_bin(num):
+        tmp = []
+        while num >= 1:
+            tmp.append(num % 2)
+            num //= 2
+
+        need_zero = n - len(tmp)
         
+        if need_zero > 0:
+            for i in range(need_zero):
+                tmp.append(0)
+        
+        return tmp[::-1]
+    
+    for num1, num2 in zip(arr1, arr2):
+        tmp_str = ""
+        for new_num1, new_num2 in zip(make_bin(num1), make_bin(num2)):
+            if new_num1 + new_num2 >= 1:
+                tmp_str += "#"
+            else:
+                tmp_str += " "
+    
+        new_arr.append(tmp_str)
+
     return new_arr
 
-
-# 이진수 만드는 함수
-def makeBinary(n, arr):
-    res = []
-    
-    for i in arr:
-        bin_arr = []
-        while i >= 2:
-            remain = i % 2
-            i = i // 2
-            bin_arr.append(remain)
-        bin_arr.append(i)
-        if len(bin_arr) != n:
-            for _ in range(n - len(bin_arr)):
-                bin_arr.append(0)
-        bin_arr.reverse()
-        res.append(bin_arr)
-
-    return res
